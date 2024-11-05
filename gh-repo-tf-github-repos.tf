c80075916c5a93e67c44e033e2f0ed27506b3efc
Erik S. Carlsten <ecarlste@gmail.com>
2024-11-05 14:31:34 -0700

    add auto delete of branch on merge (#3)
    This PR reformats a tiny bit and also changes the
    `delete_branch_on_merge` to be `true`.

--- gh-repo-tf-github-repos.tf
+++ gh-repo-tf-github-repos.tf
@@ -5,10 +5,10 @@ resource "github_repository" "tf-github-repos" {
   has_projects  = true
   has_wiki      = true
 
-  allow_merge_commit = false
-  allow_squash_merge = true
-  allow_rebase_merge = false
-
+  allow_merge_commit          = false
+  allow_squash_merge          = true
+  allow_rebase_merge          = false
   squash_merge_commit_title   = "PR_TITLE"
-  squash_merge_commit_message = "PR_MESSAGE"
+  squash_merge_commit_message = "PR_BODY"
+  delete_branch_on_merge      = true
 }
