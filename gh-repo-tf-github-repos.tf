bbddf8e295e3b9d93288a70556f6a74e3f35b365
Erik S. Carlsten <ecarlste@gmail.com>
2024-12-04 08:39:42 -0700

    added local vagrant cluster repo

--- gh-repo-tf-github-repos.tf
+++ gh-repo-tf-github-repos.tf
@@ -1,4 +1,9 @@
-module "github_repository" {
+module "tf_repos_github_repository" {
   source    = "./modules/gh-repo"
   repo_name = "tf-repos"
+}
+
+module "local_vagrant_cluster_github_repository" {
+  source    = "./modules/gh-repo"
+  repo_name = "local-vagrant-cluster"
 }
